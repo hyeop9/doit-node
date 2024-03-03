5c624bcfba04349d949acc9a0a0ff803e5cb8c34
Hyunyeop Ahn <hyeop9@gmail.com>
2024-03-03 17:29:18 +0900

    비동기 처리 , 콜백 함수

--- basics/02/sync.js
+++ basics/02/sync.js
@@ -1,1 +1,24 @@
 // 순서대로 실행하기  (결과 비교: 02\results\sync.js)
+function displayA() {
+    console.log('A');
+}
+
+function displayB(callback) {
+    setTimeout(() => {
+        console.log('B');
+        callback();
+    }, 2000);
+}
+function displayC() {
+    console.log('C');
+}
+
+displayA();
+// displayB();
+// displayC();
+
+/**
+ * 비동기 처리 : 원하는 순서에 따라 처리
+ */
+// 콜백 함수
+displayB(displayC)
